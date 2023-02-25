a7cfbaf899971d3cc2bf5c07cbc3892bad37110b
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-02-25 13:03:15 -0300

    Estilização inicial

--- Formulário/src/App.jsx
+++ Formulário/src/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
+import Form from './components/Form'
+import { BsLightningCharge } from "react-icons/bs";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className='conteiner_form'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className='icon_thunder'><BsLightningCharge className='icon'/></div>
+      
+        <h1>Faça sua inscrição para o <span>futuro</span></h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form />
     </div>
   )
 }
