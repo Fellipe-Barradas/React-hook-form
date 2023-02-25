44f93faaaa05fa4a59da0326b232bdf83c0d1f21
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-02-25 15:12:18 -0300

    Utilizado o react-hook-form

--- Formulário/src/components/Form.jsx
+++ Formulário/src/components/Form.jsx
@@ -1,14 +1,32 @@
-import { useForm, useWatch } from "react-hook-form";
+import { useForm} from "react-hook-form";
 import styles from "./Form.module.css"
 function Form() {
+    //Usando o hook para registrar os dados fornecidos
+    const {register, handleSubmit, formState:{ errors }} = useForm()
+
+    const onSubmit = (e)=>{
+        console.log(e)
+    } 
+    
     return ( 
         <>
-        <form className={styles.form}>
-          <div className={styles.content}>
+        <div className={styles.form}>
+          <div className={styles.content} >
             <label htmlFor="name">Nome:</label>
-            <input type="text" name="name" id="name" placeholder="Digite seu nome..." autoComplete="off"/>
+            <input 
+            type="text" 
+            name="name" 
+            id="name"  
+            placeholder="Digite seu nome..." 
+            autoComplete="off"
+            {...register('name',{ required:true,})}
+            />
+            {errors?.name && <p className={styles.errorMsg}>Adicione um nome válido</p> }
           </div>
-        </form>
+          <div>
+            <button onClick={()=>handleSubmit(onSubmit)()}>Enviar</button>
+          </div>
+        </div>
         </>
      );
 }
